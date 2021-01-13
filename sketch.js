9a2f9b74d8c4d0349e84b286a2ec77384db46e82
Saloni83 <75675666+Saloni83@users.noreply.github.com>
2021-01-13 15:05:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ground, gameState,engine, world,dustbin,paper;
+function setup() {
+  createCanvas(800, 400);
+  rectMode(CENTER);
+
+  gameState = "start";
+
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+
+  dustbin = new DustBin(720, 390, 100, 10);
+  paper = new Paper(100, 300, 10);
+  ground = Bodies.rectangle(width / 2, 400, width, 10,
+  {
+    isStatic: true
+  });
+  World.add(world, ground);
+}
+
+function draw() {
+  if (gameState === "start") {
+    background("darkblue");
+    textSize(20);
+    fill("yellow");
+    text("LET'S TRASH THE TRASH HABIT BY THIS GAME", 100, 200)
+    if (keyCode === UP_ARROW) {
+      gameState = "play"
+    }
+  }
+  if (gameState === "play") {
+    rectMode(CENTER);
+    background(0);
+    dustbin.display();
+    paper.display();
+
+  }
+}
+
+
+function keyPressed(){
+  if (keyCode === UP_ARROW && gameState === "play") {
+    Matter.Body.applyForce(paper.body, paper.body.position, {
+      x: 7,
+      y: -10
+    });
+  }
+}
